d32a0c63bacb8fb12537bb7fc00a1b7e05f91179
Crypthas <46863229+Crypthas@users.noreply.github.com>
2020-10-24 22:59:29 +0300

    Update crypt.py

--- crypt.py
+++ crypt.py
@@ -1,15 +1,13 @@
 import os
 import argparse
-
 from Crypto.Cipher import AES
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='AES CBC encryption of files.')
-    parser.add_argument('-o', '--object_path', required=True, help='Path to object for encryption.')
+    parser.add_argument('-f', '--files', required=True, help='Path to object for encryption.')
     parser.add_argument('-k', '--key', required=True, help='Key for encryption (MAX: 32 letters).')
     parser.add_argument('-d', '--decryption', action='store_true', help='Decryption mode.')
-    parser.add_argument('-p', '--path_to_copy', default='(Replace object)', help='Path to copy object.')
     return parser.parse_args()
 
 
@@ -21,15 +19,12 @@ class Crypt:
         self.__block_size = 16
         self.__key = self.__enc_pad(args.key.encode())
         self.__iv = self.__key[:16]
-        self.__object_path = args.object_path
+        self.__object_path = args.files
         if not args.decryption:
             self.__mode = 'encryption'
         else:
             self.__mode = 'decryption'
-        if args.path_to_copy == '(Replace object)':
-            self.__new_object_path = self.__object_path
-        else:
-            self.__new_object_path = args.path_to_copy
+
 
 
     def __enc_pad(self, data):
@@ -100,11 +95,9 @@ class Crypt:
                 padded_data = self.__enc_pad(content)
                 encrypted_data = self.__encrypted_marker + self.__encrypt(padded_data)
 
-                if self.__object_path != self.__new_object_path:
-                    path = path.replace(object_path, self.__new_object_path)
 
-                    if not os.path.exists(os.path.dirname(path)):
-                        os.makedirs(os.path.dirname(path))
+                if not os.path.exists(os.path.dirname(path)):
+                    os.makedirs(os.path.dirname(path))
 
                 self.__create_file(path, encrypted_data)
 
@@ -140,11 +133,9 @@ class Crypt:
                 else:
                     unpadded_data = unpadded_data[len(self.__flag):]
 
-                if self.__object_path != self.__new_object_path:
-                    path = path.replace(object_path, self.__new_object_path)
 
-                    if not os.path.exists(os.path.dirname(path)):
-                        os.makedirs(os.path.dirname(path))
+                if not os.path.exists(os.path.dirname(path)):
+                    os.makedirs(os.path.dirname(path))
 
                 self.__create_file(path, unpadded_data)
 
@@ -165,11 +156,8 @@ class Crypt:
         data = self.__flag + data
         padded_data = self.__enc_pad(data)
         encrypted_data = self.__encrypted_marker + self.__encrypt(padded_data)
-        self.__create_file(self.__new_object_path, encrypted_data)
-        if self.__object_path == self.__new_object_path:
-            print(self.__object_path, 'has been successfully encrypted.')
-        else:
-            print(self.__object_path, '[decrypted] ->', self.__new_object_path, '[encrypted].')
+        self.__create_file(self.__object_path, encrypted_data)
+        print(self.__object_path, 'has been successfully encrypted.')
 
 
     def __decrypt_file(self, object_name):
@@ -182,11 +170,9 @@ class Crypt:
             exit('Incorrect key.')
         else:
             unpadded_data = unpadded_data[len(self.__flag):]
-        self.__create_file(self.__new_object_path, unpadded_data)
-        if self.__object_path == self.__new_object_path:
-            print(self.__object_path, 'has been successfully decrypted.')
-        else:
-            print(self.__object_path, '[encrypted] ->', self.__new_object_path, '[decrypted].')
+        self.__create_file(self.__object_path, unpadded_data)
+        print(self.__object_path, 'has been successfully decrypted.')
+
 
 
     def run(self):
